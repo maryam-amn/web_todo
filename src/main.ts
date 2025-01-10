2d0c311da3c885530ef7ec1faac4cee915818c4f
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-01-10 11:10:18 +0100

    feat: creation of the  to-do, we can add new to do close #6

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
 import './style.css'
 
 console.log('Hello from typescript')
+
+const TodoInput = document.querySelector<HTMLInputElement>('#todo-input')
+const button = document.querySelector<HTMLButtonElement>('#add-todo-button')
+const outputList = document.querySelector<HTMLUListElement>('#todo-list')
+
+function myList() {
+  const addedTodoText = TodoInput?.value
+  if (addedTodoText && outputList) {
+    const newList = document.createElement('li')
+    newList.textContent = addedTodoText
+    outputList.appendChild(newList)
+  } else {
+    alert('Please enter a todo ')
+  }
+}
+if (button && TodoInput && outputList) {
+  button.addEventListener('click', () => {
+    myList()
+  })
+
+  TodoInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      myList()
+    }
+  })
+}
