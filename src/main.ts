287610e271cd1c7f0dd22da57e85e2e0bf53e817
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-01-13 10:56:24 +0100

    feat: the todos can be stock in a local storage

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,21 @@ console.log('Hello from typescript')
 const todoInput = document.querySelector<HTMLInputElement>('#todo-input')
 const button = document.querySelector<HTMLButtonElement>('#add-todo-button')
 const outputList = document.querySelector<HTMLUListElement>('#todo-list')
+const deserialized = localStorage.getItem('value')
 
-function myList() {
-  if (todoInput && outputList) {
-    const addedTodoText = todoInput.value
+interface Todo {
+  text: string
+}
+
+let todos: Todo[] = []
+if (deserialized) {
+  todos = JSON.parse(deserialized)
+  todos.forEach(myList)
+}
+
+function myList(todo: Todo) {
+  if (outputList) {
+    const addedTodoText = todo.text
     const newList = document.createElement('li')
     newList.textContent = addedTodoText
     outputList.appendChild(newList)
@@ -16,14 +27,30 @@ function myList() {
     alert('Please enter a todo ')
   }
 }
+
 if (button && todoInput && outputList) {
   button.addEventListener('click', () => {
-    myList()
+    test()
   })
 
-  todoInput.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      myList()
+  todoInput.addEventListener('keydown', (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      test()
     }
   })
+}
+
+// Define your function
+function test(): void {
+  if (todoInput) {
+    const text: string = todoInput.value.trim()
+    if (text) {
+      const newTodo: Todo = { text }
+      todos.push(newTodo)
+      const serialized = JSON.stringify(todos)
+      localStorage.setItem('value', serialized)
+      myList(newTodo)
+      todoInput.value = ''
+    }
+  }
 }
